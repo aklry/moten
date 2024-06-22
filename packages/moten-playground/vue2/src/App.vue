f0c5bbca99393f539672f0ab0a7ea757860ff940
Aklry <74409739+aklry@users.noreply.github.com>
2024-06-22 19:51:32 +0800

    feat: 完成低代码平台vue2和vue3版本的适配(通过monorepo架构)

---
+++ packages/moten-playground/vue2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div>
+      <mo-image src="https://inews.gtimg.com/news_bt/OWvBqEB-jc6Q1r2UhKaeRr-uG6t39aEkVUds-OC0VNv4oAA/641" />
+    </div>
+  </div>
+</template>
+
+<script>
+import moten from '@moten/ui/vue2'
+const { MoImage } = moten
+
+export default {
+  name: 'App',
+  components: {
+    MoImage
+  }
+}
+</script>
+
+<style></style>

6921207c15a8b26a8a4177f4bcb6f9d87ae51144
Aklry <74409739+aklry@users.noreply.github.com>
2024-06-24 19:29:22 +0800

    feat(): 实现git检测工具以及渲染平台项目初始化

---
+++ packages/moten-admin/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home-container">
+    <h1>Welcome to Moten Admin</h1>
+  </div>
+</template>
---
+++ packages/moten-admin/vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      dts: fileURLToPath(new URL('./types/auto-imports.d.ts', import.meta.url))
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dirs: ['src/components'],
+      dts: fileURLToPath(new URL('./types/components.d.ts', import.meta.url)),
+      extensions: ['vue'],
+      deep: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})

574bd5f706f0686ca09afb539763ddbfbe955582
Aklry <74409739+aklry@users.noreply.github.com>
2024-07-07 22:26:28 +0800

    feat(@moten/ui): 使用sass封装响应式，实现低代码平台渲染区pc和移动端的切换

--- packages/moten-playground/vue3/src/App.vue
+++ packages/moten-playground/vue3/src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 import moten from '@moten/ui'
+import { ref } from 'vue'
 const { MoImage } = moten
+
+const width = ref('')
+const changeWidth = () => {
+  width.value = '300px'
+}
 </script>
 
 <template>
-  <mo-image src="http://quiz.aklry.com/logo.png" />
+  <div class="container" :style="{ width }">
+    <mo-image src="http://quiz.aklry.com/logo.png" @click="changeWidth" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  container-type: inline-size;
+  width: 100%;
+}
+</style>
--- packages/moten-ui/src/components/mo-image.vue
+++ packages/moten-ui/src/components/mo-image.vue
@@ -19,13 +19,24 @@ export default defineComponent({
 </script>
 <template>
   <div class="mo-image-container">
-    <img :src="src" alt="image" class="image" />
+    <h1 class="title">标题</h1>
+    <img :src="src" alt="image" class="image m-1" />
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .image {
   width: 200px;
   height: 200px;
+}
+.title {
+  font-size: 50px;
+  color: red;
+}
+@container (max-width: 600px) {
+  .title {
+    font-size: 20px;
+    color: blue;
+  }
 }
 </style>
